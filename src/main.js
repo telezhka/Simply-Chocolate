e1fae13b6984ec2ad5696a920bda84f628e35d13
Roman Telik <rtelik58@gmail.com>
2023-07-19 23:49:54 +0300

    swiper

--- src/main.js
+++ src/main.js
@@ -5,5 +5,5 @@ import './js/mobile-menu';
 import './js/modalsubscribe';
 import './js/slider-swiper';
 import './js/slider';
-import './js/slick';
-import './js/script';
+// import './js/slick';
+// import './js/script';
