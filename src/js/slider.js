25b2cbc2b5cd4d24e6e3c168f5f7d6bcf6527275
Roman Telik <rtelik58@gmail.com>
2023-07-20 00:20:37 +0300

    fix-modal-first-att

--- src/js/slider.js
+++ src/js/slider.js
@@ -63,3 +63,34 @@ const productsSwiper = new Swiper('.products-swiper', {
     },
   },
 });
+
+const buyNowSwiper = new Swiper('.buy-now-swiper', {
+  spaceBetween: 18,
+  // loop: true,
+
+  // autoplay: {
+  //   delay: 2000,
+  //   disableOnInteraction: false,
+  // },
+
+  // If we need pagination
+  pagination: {
+    el: '.review-swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2.5,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
